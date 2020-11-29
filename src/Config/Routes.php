50402d47e1678debd2fb8ae3e088e93e2db71e55
codenomdev <falachul14@gmail.com>
2020-11-29 15:51:22 +0700

    Initial Commit
    Initial Commit

---
+++ src/Config/Routes.php
@@ -0,0 +1,4 @@
+<?php
+
+$routes->group('midtrans', function ($routes) {
+});
---
+++ src/Controllers/Snap/Snap.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Codenom\MidtransSampleData\Controllers;
+
+use CodeIgniter\Controller;
+
+class Snap extends Controller
+{
+    public function index()
+    {
+    }
+}
